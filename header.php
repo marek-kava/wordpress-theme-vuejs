9da1c029e17f3af4654dd1dd2343cfd847993ead
mkava <marek@mkava.pl>
2020-01-09 00:42:52 +0100

    Fix favicon & meta title

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
-    <meta
-        charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Example APP</title>
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
     <?php wp_head(); ?>
 </head>
 
